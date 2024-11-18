487356193bd6bba9a7c87e481ed1fa97bd106369
RostykGnat <149826109+RostykGnat@users.noreply.github.com>
2024-11-18 18:47:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Створити телефонну книгу створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+// class Abonent {
+//   constructor(name, phoneNumber) {
+//     this._name = name;
+//     this._phoneNumber = phoneNumber;
+//   }
+//   set updatePhoneNumber(newPhoneNumber) {
+//     this._phoneNumber = newPhoneNumber;
+//   }
+
+//   get info() {
+//     return `Ім'я: ${this._name}, Номер телефону: ${this._phoneNumber}`;
+//   }
+// }
+// const abonent1 = new Abonent("Ростислав", "0981234777");
+// const abonent2 = new Abonent("Маша", "011111111");
+// const abonent3 = new Abonent("Настя", "0101010101");
+// console.log(abonent1.info);
+// console.log(abonent2.info);
+// console.log(abonent3.info);
